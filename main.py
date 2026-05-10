f8dab49807d1960b3cde90445e11f3be5e3e3b2c
vera1956des-ops <vera1956des@gmail.com>
2026-05-10 05:36:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
 import asyncio
-import random
-import hashlib
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import InlineQueryResultArticle, InputTextMessageContent
 from aiohttp import web
 
-API_TOKEN = '8734155157:AAF7SBBYKtiAzZ7M3Ye5UDdwBQ0K5p8caJk'
+API_TOKEN='8734155157:AAFDBBUKtiAzZ7M3Ye5UDwBQ0Kp8caJk'
+bot=Bot(token=API_TOKEN) 
+dp=Dispatcher()
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+@dp.inline_query() 
+async def i_q(q:types.InlineQuery) :
+    res=[types.InlineQueryResultArticle(
+        id='1', title='КТО Я?', 
+        input_message_content=types.InputTextMessageContent(
+            message_text='Я сегодня Сигма! ') 
+    ) ]
+    await q. answer(res, cache_time=1) 
 
-@dp.inline_query()
-async def inline_handler(query: types.InlineQuery):
-    results = [
-        InlineQueryResultArticle(
-            id=hashlib.md5(str(random.random()).encode()).hexdigest(),
-            title="КТО Я СЕГОДНЯ?",
-            input_message_content=InputTextMessageContent(message_text=f"Я сегодня: {random.choice(['Сигма','Скуф','Тюбик'])}")
-        )
-       ]
-    await query.answer(results, cache_time=1)
-  
-async def handle(request):
-    return web.Response(text="OK")
-  
+async def h(req) :
+    return web. Response(text="OK") 
+
 async def main():
-    app = web.Application()
-    app.router.add_get('/', handle)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', int(os.getenv('PORT', 10000)))
-    await site.start()
-    await dp.start_polling(bot)
-  
-if __name__ == '__main__':
-    asyncio.run(main())
+    app=web.Application() 
+    runner=web.AppRunner(app) 
+    await runner. setup() 
+    await web. TCPSite(runner,'0.0.0.0', int(os.getenv('PORT', 10000))).start() 
+    await dp. start_polling(bot) 
+
+if __name__=='__main__':
+    asyncio.run(main()) 
